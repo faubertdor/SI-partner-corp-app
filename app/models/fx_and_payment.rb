e8e06e6251b4c897bc01053dd22111151b95268a
Jean Faubert Dorancy <faubertdor@gmail.com>
2016-03-31 15:53:30 -0400

    Added validation to fx_payment and politically exposed people.

--- app/models/fx_and_payment.rb
+++ app/models/fx_and_payment.rb
@@ -1,3 +1,6 @@
 class FxAndPayment < ActiveRecord::Base
   belongs_to :user
+  validates :purpose, :currencies, :approx_trade_vol, :approx_monthly_nb,
+            :approx_annual_vol, :countries_transf_to,
+            :countries_transf_from, presence: true
 end
--- app/models/politically_exposed_person.rb
+++ app/models/politically_exposed_person.rb
@@ -1,3 +1,4 @@
 class PoliticallyExposedPerson < ActiveRecord::Base
   belongs_to :user
+  validates :name, :title, :position_held, :from, :to, presence: true
 end
